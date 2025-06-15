46c73235598f8bb1b4379538eabd23184b416049
Ingvar <jamessoer91@gmail.com>
2025-06-15 14:25:33 +0300

    weather render 3

--- init.js
+++ init.js
@@ -2,9 +2,11 @@
 import { switchTheme } from "./src/components/switchTheme.js";
 import { getGeoData } from "./src/api/geoData.js";
 import { getWeatherByForm } from "./src/components/inputForm.js";
+import { renderCurrentTime } from "./src/helpers/currentTime.js";
 
 export function initApp() {
   switchTheme();
   getGeoData();
   getWeatherByForm();
+  renderCurrentTime();
 }
